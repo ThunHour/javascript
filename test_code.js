09238dd6eaef9a627fe9a1d2074f453d60ab125a
Cipher Zernia <88335360+ThunHour@users.noreply.github.com>
2021-10-28 20:26:23 +0700

    Delete test_code.js

--- test_code.js
+++
@@ -1,9 +0,0 @@
-// var word='dhaskj&&*_TFV%@bd'
-// // var spacr=''
-// // for (let i=0; i<word.length-5; i++) {
-// //     console.log(word[i]);
-// //     spacr+= word[i];
-// // }
-// // console.log(spacr);
-// console.log(word[1])
-// console.log(word.length)
